028078669e62c0c9c23fd824e71a42357c0b13b4
beniaminstan <beniaminstan@gmail.com>
2020-12-17 16:59:55 +0100

    Added DROP USER

--- sql/privileges.sql
+++ sql/privileges.sql
@@ -1,10 +1,16 @@
+DROP USER 'potentialClient'@'localhost';
+
 CREATE USER 'potentialClient'@'localhost' IDENTIFIED BY 'potentialClient';
 
 GRANT SELECT, INSERT ON pchardwarev1.* TO 'potentialClient'@'localhost';
 
+   DROP USER 'administrator'@'localhost';
+
 CREATE USER 'administrator'@'localhost' IDENTIFIED BY 'administrator';
 
 GRANT ALL PRIVILEGES ON pchardwarev1.* TO 'administrator'@'localhost';
+
+       DROP USER 'employee'@'localhost';
 
 CREATE USER 'employee'@'localhost' IDENTIFIED BY 'employee';
 
